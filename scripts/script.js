95219683c71fefcfcc13c28c49f0f903bbce3e5d
mrogovets <34492317+mrogovets@users.noreply.github.com>
2021-03-09 20:57:58 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+
+  const tabs = () => {
+    const cardDetailChangeElems = document.querySelectorAll(
+      ".card-detail__change"
+    );
+    const cardDetailsTitleElem = document.querySelector(".card-details__title");
+    const cardImageItemElem = document.querySelector(".card__image_item");
+    const cardDetailsPriceElem = document.querySelector(".card-details__price");
+    const descriptionMemory = document.querySelector(".description__memory");
+
+    const data = [
+      {
+        name: "Смартфон Apple iPhone 12 Pro 128GB Graphite",
+        img: "img/iPhone-graphite.png",
+        price: 95990,
+        memoryROM: 128,
+      },
+      {
+        name: "Смартфон Apple iPhone 12 Pro 128GB Silver",
+        img: "img/iPhone-silver.png",
+        price: 120990,
+        memoryROM: 256,
+      },
+      {
+        name: "Смартфон Apple iPhone 12 Pro 128GB Pacific Blue",
+        img: "img/iPhone-blue.png",
+        price: 99990,
+        memoryROM: 128,
+      },
+    ];
+    cardDetailChangeElems.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        if (!btn.classList.contains("active")) {
+        }
+      });
+    });
+  };
+
+  tabs();
+});
