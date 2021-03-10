8dec65917358bdbfe7971c2248e79d61edf50a2d
mrogovets <mrogovets@ukr.net>
2021-03-10 21:26:28 +0200

    added accordion and modal

--- scripts/script.js
+++ scripts/script.js
@@ -50,5 +50,71 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   };
 
+  const accordion = () => {
+    const characteristicsListElem = document.querySelector(
+      ".characteristics__list"
+    );
+    const characteristicsItemElems = document.querySelectorAll(
+      ".characteristics__item"
+    );
+
+    const open = (button, dropDown) => {
+      closeAllDrops();
+      dropDown.style.height = `${dropDown.scrollHeight}px`;
+      button.classList.add("active");
+      dropDown.classList.add("active");
+    };
+    const close = (button, dropDown) => {
+      button.classList.remove("active");
+      dropDown.classList.remove("active");
+      dropDown.style.height = "";
+    };
+
+    const closeAllDrops = (button, dropDown) => {
+      characteristicsItemElems.forEach((elem) => {
+        if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+          close(elem.children[0], elem.children[1]);
+        }
+      });
+    };
+
+    characteristicsListElem.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.classList.contains("characteristics__title")) {
+        const parent = target.closest(".characteristics__item");
+        const description = parent.querySelector(
+          ".characteristics__description"
+        );
+        description.classList.contains("active")
+          ? close(target, description)
+          : open(target, description);
+      }
+    });
+    document.body.addEventListener("click", (event) => {
+      const target = event.target;
+      if (!target.closest(".characteristics__list")) {
+        closeAllDrops();
+      }
+    });
+  };
+
+  const modal = () => {
+    const cardDetailsButtonBuy = document.querySelector(
+      ".card-details__button_buy"
+    );
+    const modal = document.querySelector(".modal");
+    cardDetailsButtonBuy.addEventListener("click", () => {
+      modal.classList.add("open");
+    });
+    modal.addEventListener("click", (event) => {
+      const target = event.target;
+      if (target.classList.contains("modal__close")) {
+        modal.classList.remove("open");
+      }
+    });
+  };
+
   tabs();
+  accordion();
+  modal();
 });
